44dec03ba7b87ae7a47945ef50a4c7f9a4be8125
Oliver Lehner <lehner.oliver@gmail.com>
2017-07-17 23:21:43 +0200

    dog show page done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    offset = rand(Dog.count)
+    # Rails 4
+    @dog = Dog.offset(offset).first
   end
 end
